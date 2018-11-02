6a9efc029836ca18e73a6a959f6624d8389a8346
Phung Lai <tl353@njit.edu>
2018-11-02 18:23:41 -0400

    Task: Step 13 - Unit test to insert a user. Edit: Feature test and Update user table that is missing in previous steps

--- tests/Feature/AboutPageTest.php
+++ tests/Feature/AboutPageTest.php
@@ -3,21 +3,21 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class AboutPageTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testAboutTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/about');
 
         $response->assertStatus(200);
     }
-
 
 }
--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -3,21 +3,20 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class RegisterTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testRegisterTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/register');
 
         $response->assertStatus(200);
     }
-
-
 }
